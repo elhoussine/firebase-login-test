e66531e882f82c7f14f660333c305b3804310698
elhoussine <elhoussinetalab@gmail.com>
2015-11-02 23:31:07 +0400

    whole new level! added new approach to retrieve data (surprisingly simple) and barcode scanner

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,4 +1,4 @@
-var cApp = angular.module('starter.controllers', ['ionic', 'firebase']);
+var cApp = angular.module('starter.controllers', ['ionic', 'firebase', 'ngCordova']);
 
 // irebase app ref 
 var myFirebaseRef = new Firebase("https://logintest103.firebaseio.com/");
@@ -31,8 +31,10 @@ var uid = null;
 // sign up controller 
 cApp.controller('SignupCtrl', function($scope) { 
 
+
   // storing signup credenttials in object 
   $scope.signupData = {};
+
 
   // sign up function 
   $scope.signup = function () { 
@@ -63,7 +65,7 @@ cApp.controller('SignupCtrl', function($scope) {
 
 
 // login controller 
-cApp.controller('LoginCtrl', function($scope, $rootScope, $ionicPopup, $state, Items, $ionicHistory, $ionicLoading) { 
+cApp.controller('LoginCtrl', function($scope, $rootScope, $ionicPopup, $state, $ionicHistory, $ionicLoading) { 
 
   // clears nav history so we dont have 'back' button when we logout
   $ionicHistory.clearHistory();
@@ -89,12 +91,6 @@ cApp.controller('LoginCtrl', function($scope, $rootScope, $ionicPopup, $state, I
       // login successfull
       console.log("authenticated succcessfully with payload: ", authData);
 
-      // saving user id so we save items to his id later
-      $rootScope.uid = authData.uid;
-
-      // for debuggin 
-      console.log("User Id Stored is: ", $rootScope.uid)
-
       // cleating input data
       $scope.clearFields();
 
@@ -118,110 +114,7 @@ cApp.controller('LoginCtrl', function($scope, $rootScope, $ionicPopup, $state, I
 
       // the auth handler gets executed
     }, authHandler);
-
-
   };
-
-
-  // injecting items factory as a dependency 
-  $scope.items = Items;
-  
-
-  // function that adds item to firebase Databse. !! note the Items Factory !! 
-  $scope.addItem = function() {
-
-    // this prompts the user to enter name item
-    var name = prompt("What do you need to buy?");
-
-    // if the user entered a value
-    if (name) {
-
-      // the real code for adding the item
-      // first we call the dependency 
-      // then we call $add which is prefefined in $firebaseArray that we userd in the factory 
-      // the add the details, note that you can as much details as you want. 
-      $scope.items.$add({
-      	"name": name, 
-      	"uid": $scope.uid
-      });
-    }
-  };
-
-  // retreive items testing
-
-  $scope.getItems = function () { 
-  	console.log("get items button clicked");
-
-    // reference to database named Items 
-    var ref = new Firebase("https://logintest103.firebaseio.com/Items");
-
-    // the query 
-    // !! child means the property !! 
-    // on is triggering the query
-    ref.orderByChild("uid").equalTo($scope.uid).on("child_added", function(snapshot) { 
-    	console.log(snapshot.key());
-    });
-  };
-
-  // testing another way to save objects
-
-    // I will create databes called phones
-    var ref2 = new Firebase("https://logintest103.firebaseio.com/");
-    var usersRef = ref2.child("phones"); 
-
-    $scope.anotherAdd = function () { 
-
-   // adding phones individialy
-   usersRef.child("iPhone 6").set({
-    // here is the json data
-    // just fill it with properities and its values 
-    model: "Plus", 
-    storage: "16GB", 
-    resolution: "retina"
-  });
-
-   usersRef.child("Samsugn S5").set({ 
-   	model: "Normal",
-   	storage: "32GB", 
-   	resolution: "Shitty"
-   });
-
- };
-
-// justing adding form to the previous technique 
-$scope.newPhone = {};
-
-$scope.addPhone = function () { 
-  // refereance to database location 
-  usersRef.child($scope.newPhone.name).set({ 
-  	model: $scope.newPhone.model, 
-  	storage: $scope.newPhone.storage, 
-  	resolution: $scope.newPhone.resolution,
-  	uid: $scope.uid
-
-  });
-
-  console.log("Phone added successfully!");
-};
-
-
-// function to get phones with current user id 
-$scope.getPhone = function () { 
-  // ref to data location 
-  var phonesRef = new Firebase("https://logintest103.firebaseio.com/phones"); 
-
-  // query 
-  phonesRef.orderByChild("uid").equalTo($scope.uid).on("child_added", function(snapshot) { 
-    // do something with the snapshots 
-    // enum throught the snapshots 
-    console.log(snapshot.key());
-    // snapshots.forEach(function(data) { 
-
-    //   // do something with this particular snapshot
-    //   console.log("Item retrieved is: ", data.key());
-    // });
-});
-};  
 });
 
 
@@ -275,11 +168,15 @@ cApp.controller('ModalCtrl', function($scope, $rootScope) {
 
 
 // home page controller 
-cApp.controller('HomeCtrl', function($scope, $rootScope, $state, $ionicModal, Auth) { 
+cApp.controller('HomeCtrl', function($cordovaBarcodeScanner, $scope, $rootScope, $state, $ionicModal, Auth, $firebaseArray) { 
+
+
+  // debugging
+  console.log("inside HomeCtrl"); 
+
 
   // checking auth and setting aith data
   $scope.auth = Auth; 
-
   $scope.auth.$onAuth(function(authData) { 
     $scope.authData = authData;
     if ($scope.authData == null) { 
@@ -301,32 +198,24 @@ cApp.controller('HomeCtrl', function($scope, $rootScope, $state, $ionicModal, Au
   // function to logout user
   $scope.logout = function () { 
     myFirebaseRef.unauth();
-
     $state.go('login');
 
     // debugging purpose 
     console.log("user logged out successfully");
   };
 
+
   // array hold fetched meals
-  $scope.allMeals = [];
+  // $scope.allMeals = null;
+
 
 		// getting meals from database
 		$scope.getMeals = function () { 
-      $scope.allMeals = [];
+      // setting ref
+      var ref = new Firebase("https://logintest103.firebaseio.com/Meals");
 
-      var mealsRef = new Firebase("https://logintest103.firebaseio.com/Meals"); 
-
-      mealsRef.on("child_added", function(snapshot) {
-        var newPost = snapshot.val();
-        // console.log("Name: " + newPost.name);
-        // console.log("Price: " + newPost.price);
-
-        $scope.allMeals.push({
-          name: newPost.name, 
-          price: newPost.price
-        })
-      });
+      // setting array with all meals 
+      $scope.allMeals = $firebaseArray(ref);
     };
 
     // modal for selected meal 
@@ -408,19 +297,109 @@ cApp.controller('HomeCtrl', function($scope, $rootScope, $state, $ionicModal, Au
       // setting selected meal depending on index
       $scope.selectedMeal = $scope.allMeals[$index];
 
+      // setting fixed meal name so we can referance to it when saving 
+      $scope.fixedCurrentMeal = $scope.selectedMeal.name;
+      $scope.fixedCurrentPrice = $scope.selectedMeal.price;
+
       // showing modal for selected meal 
       $scope.selectedMealShow();
     };
 
+
+    // testing barcode scanner 
+    $scope.scanBarcode = function () { 
+      // calling cordova plugin 
+     $cordovaBarcodeScanner
+      .scan()
+      .then(function(barcodeData) {
+        // Success! Barcode data is here
+        alert(barcodeData.text);
+
+      }, function(error) {
+        // An error occurred
+        alert("error scanning");
+      });
+    };
+    
+
+    // testing encoding barcode
+    $scope.encodeBarcode = function () { 
+          // NOTE: encoding not functioning yet
+    $cordovaBarcodeScanner
+      .encode(BarcodeScanner.Encode.TEXT_TYPE, "http://www.nytimes.com")
+      .then(function(success) {
+        // Success!
+        alert("encode success: " + success);
+      }, function(error) {
+        // An error occurred
+        alert("encode failed: " + error);
+      });
+    };
   });
 
 
 cApp.controller('SelectedMealCtrl', function($scope) { 
+  // meals ref
+  var mealsRef = new Firebase("https://logintest103.firebaseio.com/Meals"); 
 
   // funcion for 'save changes' button 
   $scope.updateMeal = function () { 
+    // debugging 
     console.log("save changes have been clicked");
 
-    // dismissing modal
+    // checking if user clicked 'change' with leaving at least one field empty
+    if ($scope.selectedMeal.name == '' || $scope.selectedMeal.price == '') { 
+      // debugging
+      console.log("user left fields empty");
+
+      // showing alert to user "cannot leave empty"
+      alert("You cannot leave that empty!");
+    } else { 
+      // debuggin 
+      console.log("fields are not empty");
+
+      if ($scope.fixedCurrentPrice != $scope.selectedMeal.price && $scope.fixedCurrentMeal == $scope.selectedMeal.name) { 
+        console.log("only price changes");
+
+        // incase of change of price only 
+        // adding new item with new entries 
+        mealsRef.child($scope.selectedMeal.name).update({
+          price: $scope.selectedMeal.price
+        });
+      } else {
+        console.log("not only price changes");
+        // starting the update process      
+        // adding new item with new entries 
+        mealsRef.child($scope.selectedMeal.name).set({
+         name: $scope.selectedMeal.name,
+         price: $scope.selectedMeal.price, 
+         uid: uid
+      });
+
+      // deleting current piece of yack 
+      var toDelete = mealsRef.child($scope.fixedCurrentMeal);
+      toDelete.remove();
+    }
+
+      // dismissing modal
+      $scope.closeMealModal(); 
+    }
+  };
+
+
+  // function to delete meal 
+  $scope.deleteMeal = function () { 
+    // deleting current piece of yack 
+    var toDelete = mealsRef.child($scope.selectedMeal.name);
+    toDelete.remove(function (error) { 
+      if (error) { 
+        console.log("error deleting");
+      } else { 
+        console.log("deleted successfully"); 
+
+      // dismissing modal
+      $scope.closeMealModal();         
+    }
+  });
   };
 });
